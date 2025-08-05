3a54555b1c1d31c89e85591fe5f7cfd7bdf2c580
Ayrton <ayrton.andrade1@gmail.com>
2025-08-05 14:17:21 -0300

    feat: Add current time in home page

--- src/main/java/com/example/mvc/controllers/HomeController.java
+++ src/main/java/com/example/mvc/controllers/HomeController.java
@@ -1,13 +1,21 @@
 package com.example.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.Instant;
 
 @Controller
 public class HomeController {
 
+    // Model is used to send information to the html page
     @RequestMapping("/home")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+
+        // The attribute name should have the same name that is in home.html
+        model.addAttribute("welcomeMessage", Instant.now().toString());
+
         return "home";
     }
 }
